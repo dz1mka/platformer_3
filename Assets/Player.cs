d0cd48121d3ade2b93c4af835fa113f1c63b0c92
dz1mka <dzimka@gmail.com>
2025-04-06 20:48:21 +0300

    3 -Player Character Flip C# Scripting

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private float movement = 0f;
     public float speed = 5f;
+    private bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
-
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+
+        if (movement < 0f && facingRight == true)
+        {
+            transform.eulerAngles = new Vector3(0, -180f, 0);
+            facingRight = false;
+        }
+        else if (movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facingRight = true;
+        }
     }
 }
